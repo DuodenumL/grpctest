7acfb8bff1cc036659994068740e6e758f307ebe
zc <greyschwinger@gmail.com>
2021-10-18 02:35:29 +0800

    pbreflect mod parse proto and communicate with grpc service

---
+++ pbreflect/parse.go
@@ -0,0 +1,40 @@
+package pbreflect
+
+import (
+	"path/filepath"
+
+	"github.com/jhump/protoreflect/desc/protoparse"
+	"github.com/pkg/errors"
+)
+
+// Parse from proto file
+func Parse(filename string) (service *Service, err error) {
+	dirname, basename := filepath.Dir(filename), filepath.Base(filename)
+	filenames, err := protoparse.ResolveFilenames([]string{dirname}, basename)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	p := protoparse.Parser{
+		ImportPaths:           []string{dirname},
+		InferImportPaths:      false,
+		IncludeSourceCodeInfo: true,
+	}
+	parsedFiles, err := p.ParseFiles(filenames...)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	if len(parsedFiles) < 1 {
+		return service, errors.New("proto file not found")
+	}
+
+	service = NewService()
+	for _, parsedFile := range parsedFiles {
+		for _, svc := range parsedFile.GetServices() {
+			for _, method := range svc.GetMethods() {
+				service.rpcs[method.GetName()] = method
+			}
+		}
+	}
+
+	return
+}
